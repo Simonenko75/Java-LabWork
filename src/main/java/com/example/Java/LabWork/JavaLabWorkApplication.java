929eb551dca0994a40e6484875d3d3954353f5e1
Simonenko75 <simonenkonikita00@gmail.com>
2023-03-04 10:09:59 +0200

    first commit

---
+++ src/main/java/com/example/Java/LabWork/JavaLabWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java.LabWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabWorkApplication.class, args);
+	}
+
+}
